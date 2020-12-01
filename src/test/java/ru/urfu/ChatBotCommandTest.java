93ee1203de840a71fda016b127d9af8d02fce619
LavrovaKs <ksyusha.lavrova.01@yandex.ru>
2020-12-01 23:07:57 +0500

    Fix command /my_point

--- src/test/java/ru/urfu/ChatBotCommandTest.java
+++ src/test/java/ru/urfu/ChatBotCommandTest.java
@@ -8,12 +8,14 @@ import java.io.IOException;
 
 /**
  * Тестирует правильность работы всех команд бота
+ *
  * @author Ксения
  */
 public class ChatBotCommandTest {
 
     ChatBot chatBot = new ChatBot();
     String chatId = "859";
+    String userName = "Telegram_Bot";
 
     /**
      * Проверка, что при старте бот присылает соответствующее сообщение
@@ -22,8 +24,12 @@ public class ChatBotCommandTest {
     public void startTest() throws IOException {
         var mes = chatBot.analyzeCommand("/start", chatId);
         Assert.assertEquals("Привет, я твой помощник в подготовке к ЕГЭ по информатике." +
-                "\nсписок доступных команд:\n/help - открыть справку\n/exercise - выбор задания" +
-                "\n/time_ex - выполнение задания на время", mes);
+                "\nсписок доступных команд:" +
+                "\n/help - открыть справку" +
+                "\n/exercise - выбор задания" +
+                "\n/time_ex - выполнение задания на время" +
+                "\n/user_name - зарегистрировать свое имя" +
+                "\n/my_point - посмотреть количество набранных баллов", mes);
     }
 
     /**
@@ -32,8 +38,12 @@ public class ChatBotCommandTest {
     @Test
     public void helpTest() throws IOException {
         var mes = chatBot.analyzeCommand("/help", chatId);
-        Assert.assertEquals("Список доступных команд: \n/help - открыть справку " +
-                "\n/exercise - выбор задания \n/time_ex - выполнение задания на время", mes);
+        Assert.assertEquals("Список доступных команд: " +
+                "\n/help - открыть справку " +
+                "\n/exercise - выбор задания " +
+                "\n/time_ex - выполнение задания на время" +
+                "\n/user_name - зарегистрировать свое имя" +
+                "\n/my_point - посмотреть количество набранных баллов", mes);
     }
 
     /**
@@ -63,5 +73,14 @@ public class ChatBotCommandTest {
         Assert.assertEquals("Введите номер задания", mes);
     }
 
-
+    /**
+     * Проверка команды /user_name
+     */
+    @Test
+    public void userNameTest() throws IOException {
+        chatBot.analyzeCommand("/user_name", chatId);
+        chatBot.analyzeCommand(userName, chatId);
+        var mes = chatBot.analyzeCommand("/my_name", chatId);
+        Assert.assertEquals(userName, mes);
+    }
 }
---
+++ src/test/java/ru/urfu/MyPointTest.java
@@ -0,0 +1,20 @@
+/*
+package ru.urfu;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class MyPointTest {
+
+    ChatBot chatBot = new ChatBot();
+    String chatId = "859";
+    String userName = "Telegram_Bot";
+
+    @Test
+    public void startPoint() {
+
+    }
+}
+*/
