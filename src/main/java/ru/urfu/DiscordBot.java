73ab4659482abcbc2c6c57b83966c7e9769a26d4
TopNewbieRus <temnyyegor@gmail.com>
2020-12-09 19:25:07 +0500

    the first try of this bot

---
+++ src/main/java/ru/urfu/DiscordBot.java
@@ -0,0 +1,81 @@
+package ru.urfu;
+
+import net.dv8tion.jda.api.*;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import javax.security.auth.login.LoginException;
+import java.io.IOException;
+
+
+public class DiscordBot extends ListenerAdapter {
+    final ChatBot chatBot = new ChatBot();
+
+    /**
+     * Тут вообщем DiscordBot просыпается и готов к работе
+     * @param args
+     * @throws LoginException
+     */
+    public static void main(String[] args) throws LoginException {
+        JDABuilder builder = new JDABuilder(AccountType.BOT);
+        String token = "Nzg1ODU2ODUwNjM2NTcwNjQ1.X898OQ.552SVaQIFste5HJn1PAKcNveH4Q";
+        builder.setToken(token);
+        builder.build();
+        builder.setStatus(OnlineStatus.IDLE);
+
+    }
+
+    /*public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event){
+        //String[] args = event.getMessage().getContentRaw().split(" ");
+        String mes = event.getMessage().getContentRaw();
+        event.getChannel().sendMessage(mes);
+
+        DIscMessage dIscMessage = event.getMessage();
+    }*/
+
+    /**
+     * Тут должна происходить магия и бот должен начать обрабатывать команды
+     * @param event
+     * @throws IOException
+     */
+    /*public void onGuildMessageReceived(MessageReceivedEvent event) throws IOException {
+        *//*
+         * Получаем объект пришедшего сообщения из объекта события.
+         *//*
+        Message message = event.getMessage();
+        *//*
+         * Получаем объект канала пришедшего сообщения.
+         *//*
+        MessageChannel channel = message.getChannel();
+        *//*
+         * Достаем содержание сообщения.
+         *//*
+        String inputMsgStr = message.getContentDisplay();
+        String text_bot = message.getContentRaw();
+
+        try {
+            inputMsgStr = chatBot.analyzeCommand(text_bot, message.getId());
+            *//*new MessageBuilder(this.dscordClient).getChannel(channel).getContent(inputMsgStr).build();*//*
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }*/
+
+    public void onMessageReceived(MessageReceivedEvent event) {
+        String arg = event.getMessage().getContentRaw();
+        String name = event.getMember().getUser().getName();
+        String massageDisc = String.valueOf(event.getMessage());
+        Message message = event.getMessage();
+        MessageChannel channel = message.getChannel();
+        String inputMsgStr = message.getContentDisplay();
+        String text_bot = message.getContentRaw();
+
+        try {
+            massageDisc = chatBot.analyzeCommand(arg,name);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
