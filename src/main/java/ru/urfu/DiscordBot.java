093afaed5cd19fe4624f351e280c696c820637fd
LavrovaKs <ksyusha.lavrova.01@yandex.ru>
2020-12-10 10:43:53 +0500

    Изменения в DiscordBot

--- src/main/java/ru/urfu/DiscordBot.java
+++ src/main/java/ru/urfu/DiscordBot.java
@@ -1,138 +1,78 @@
-/*
 package ru.urfu;
 
 import net.dv8tion.jda.api.*;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
 import javax.security.auth.login.LoginException;
 import java.io.IOException;
 
 
 public class DiscordBot extends ListenerAdapter {
     final ChatBot chatBot = new ChatBot();
+    public static String pref = "/";
 
-    */
-/**
+    /**
      * Тут вообщем DiscordBot просыпается и готов к работе
-     * @param args
-     * @throws LoginException
-     *//*
-
+     */
     public static void main(String[] args) throws LoginException {
-        JDABuilder builder = new JDABuilder(AccountType.BOT);
-        String token = "Nzg1ODU2ODUwNjM2NTcwNjQ1.X898OQ.552SVaQIFste5HJn1PAKcNveH4Q";
-        builder.setToken(token);
-        builder.build();
-        builder.setStatus(OnlineStatus.IDLE);
+        JDABuilder jdaBuilder = JDABuilder.createDefault("Nzg1ODU2ODUwNjM2NTcwNjQ1.X898OQ.FACsIzlR3hB6uo4NK91IR9xrAMw");
+        JDA jda = null;
+        DiscordBot discordBot = new DiscordBot();
+        jdaBuilder.addEventListeners(discordBot);
+        try{
+            jda = jdaBuilder.build();
+        } catch (LoginException e){
+            e.printStackTrace();
+        }
 
+        jda.getPresence().setStatus(OnlineStatus.IDLE);
+        jda.getPresence().setActivity(Activity.playing("ЕГЭ по информатике"));
     }
 
-    */
-/*public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event){
-        //String[] args = event.getMessage().getContentRaw().split(" ");
-        String mes = event.getMessage().getContentRaw();
-        event.getChannel().sendMessage(mes);
-
-        DIscMessage dIscMessage = event.getMessage();
-    }*//*
-
-
-    */
-/**
-     * Тут должна происходить магия и бот должен начать обрабатывать команды
+    /**
+     * тут получаем сообщение и передаем в чат бота чтоб все работало как надо
      * @param event
-     * @throws IOException
-     *//*
+     */
 
-    */
-/*public void onGuildMessageReceived(MessageReceivedEvent event) throws IOException {
-        *//*
-*/
-/*
-         * Получаем объект пришедшего сообщения из объекта события.
-         *//*
-*/
-/*
-        Message message = event.getMessage();
-        *//*
-*/
-/*
-         * Получаем объект канала пришедшего сообщения.
-         *//*
-*/
-/*
-        MessageChannel channel = message.getChannel();
-        *//*
-*/
-/*
-         * Достаем содержание сообщения.
-         *//*
-*/
-/*
-        String inputMsgStr = message.getContentDisplay();
-        String text_bot = message.getContentRaw();
-
-        try {
-            inputMsgStr = chatBot.analyzeCommand(text_bot, message.getId());
-            *//*
-*/
-/*new MessageBuilder(this.dscordClient).getChannel(channel).getContent(inputMsgStr).build();*//*
-*/
-/*
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }*//*
-
-
-    public void onMessageReceived(MessageReceivedEvent event) {
-        String arg = event.getMessage().getContentRaw();
-        String name = event.getMember().getUser().getName();
-        String id = event.getMessage().getId();
-        String massageDisc = event.getMessage().getContentRaw();
-        Message message = event.getMessage();
-        MessageChannel channel = message.getChannel();
-        String inputMsgStr = message.getContentDisplay();
-        String text_bot = message.getContentRaw();
-
-        try {
-            massageDisc = chatBot.analyzeCommand(arg,id);
-            event.getChannel().sendMessage(massageDisc).queue();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
-*/
- package ru.urfu;
-
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
-
-import java.io.IOException;
-
-public class DiscordBot extends ListenerAdapter {
-
-    public ChatBot chatBot = new ChatBot();
     @Override
     public void onMessageReceived(MessageReceivedEvent event){
         if (event.getAuthor().isBot()) return;
         var message = event.getMessage().getContentRaw();
         String id = event.getMessage().getId();
+        String userId = event.getAuthor().getId();
         String user = event.getAuthor().toString();
         var answer = " ";
         try {
-            answer = chatBot.analyzeCommand(message, id, user);
+            answer = chatBot.analyzeCommand(message, userId, user);
         } catch (IOException e) {
             e.printStackTrace();
         }
         var channel = event.getChannel();
         channel.sendMessage(answer).queue();
+    }
 
-        System.out.println(event.getChannel());
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+        String[] args = event.getMessage().getContentRaw().split(" ");
+        if (args[0].equalsIgnoreCase(DiscordBot.pref + "info")) {
+            EmbedBuilder info = new EmbedBuilder();
+            info.setTitle("HelpInfo");
+            info.setDescription("Привет, я твой помощник в подготовке к ЕГЭ по информатике");
+            info.setColor(0xa83299);
+            info.addField("Чтобы начать напишите /exercise", "commands \n " +
+                    "/help - открыть справку \n" +
+                    "/exercise - выбор задания \n" +
+                    "/time_ex - выполнение задания на время \n" +
+                    "/my_point - посмотреть количество набранных баллов \n" +
+                    "/mistake - анализ частых ошибок по темам \n" +
+                    "/top - вывод лидеров рейтинга", false);
+
+            event.getChannel().sendMessage(info.build()).queue();
+            info.clear();
+        }
     }
 }
