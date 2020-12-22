bf8d94f8e5fd3d09ec0cada601d95f817d887618
LavrovaKs <ksyusha.lavrova.01@yandex.ru>
2020-12-22 21:07:27 +0500

    Изменение способа храниения задач по темам в ListTopic, добавление javadoc

--- src/main/java/ru/urfu/ListTopic.java
+++ src/main/java/ru/urfu/ListTopic.java
@@ -1,7 +1,10 @@
 package ru.urfu;
 
-import java.util.Arrays;
+import java.util.HashMap;
 
+/**
+ * Класс ведет подсчет ошибок
+ */
 public class ListTopic {
 
     private static int info = 0;
@@ -12,13 +15,7 @@ public class ListTopic {
     private static int game = 0;
     private static int program = 0;
 
-    private static final String[] infoList = {"4", "7", "8", "11"};
-    private static final String[] systemsList = {"14"};
-    private static final String[] logicsList = {"2", "15"};
-    private static final String[] userList = {"1", "3", "9", "10", "13"};
-    private static final String[] algoList = {"5", "12", "16", "18"};
-    private static final String[] gameList = {"19", "20", "21"};
-    private static final String[] programList = {"6", "17", "22", "23"};
+    private final HashMap<Integer, String> topics;
 
     /**
      * Метод анализирует ошибки
@@ -26,20 +23,51 @@ public class ListTopic {
      * @param ex номер задания
      */
     public void analyzeMistake(String ex) {
-        if (Arrays.binarySearch(userList, ex) >= 0)
-            user = user + 1;
-        if (Arrays.binarySearch(infoList, ex) >= 0)
-            info = info + 1;
-        if (Arrays.binarySearch(systemsList, ex) >= 0)
-            systems = systems + 1;
-        if (Arrays.binarySearch(logicsList, ex) >= 0)
-            logics = logics + 1;
-        if (Arrays.binarySearch(algoList, ex) >= 0)
+        var topic = topics.get(Integer.parseInt(ex));
+        if (topic.equals("algo"))
             algo = algo + 1;
-        if (Arrays.binarySearch(gameList, ex) >= 0)
+        if (topic.equals("user"))
+            user = user + 1;
+        if (topic.equals("logics"))
+            logics = logics + 1;
+        if (topic.equals("game"))
             game = game + 1;
-        if (Arrays.binarySearch(programList, ex) >= 0)
+        if (topic.equals("info"))
+            info = info + 1;
+        if (topic.equals("systems"))
+            systems = systems + 1;
+        if (topic.equals("program"))
             program = program + 1;
+    }
+
+    /**
+     * Разобъем номера задач по темам
+     */
+    ListTopic() {
+        topics = new HashMap<>();
+        topics.put(1, "user");
+        topics.put(2, "logics");
+        topics.put(3, "user");
+        topics.put(4, "info");
+        topics.put(5, "algo");
+        topics.put(6, "program");
+        topics.put(7, "info");
+        topics.put(8, "info");
+        topics.put(9, "user");
+        topics.put(10, "user");
+        topics.put(11, "info");
+        topics.put(12, "algo");
+        topics.put(13, "user");
+        topics.put(14, "systems");
+        topics.put(15, "logics");
+        topics.put(16, "algo");
+        topics.put(17, "program");
+        topics.put(18, "algo");
+        topics.put(19, "game");
+        topics.put(20, "game");
+        topics.put(21, "game");
+        topics.put(22, "program");
+        topics.put(23, "program");
     }
 
     /**
